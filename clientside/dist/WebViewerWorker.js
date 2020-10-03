6df9d45a44edda5c5f7a2e410930cd8a479b6ac6
Amir Aslan Aslani <aa.aslani_6@yahoo.com>
2020-10-03 19:16:30 +0330

    Add canvas-drawer as a package, instead of using final js file of that

--- clientside/dist/WebViewerWorker.js
+++ clientside/dist/WebViewerWorker.js
@@ -1,11 +1,11 @@
 /*!
- * RCRS Web Viewer v0.2.1601730499334
+ * RCRS Web Viewer v0.2.1601739821197
  * https://github.com/roborescue/rcrs-web-viewer
  * 
  * Released under the BSD-3-Clause license
  * https://opensource.org/licenses/BSD-3-Clause
  *
- * Date: 2020-10-03T13:08:19.334Z (Sat, 03 Oct 2020 13:08:19 GMT)
+ * Date: 2020-10-03T15:43:41.197Z (Sat, 03 Oct 2020 15:43:41 GMT)
  */
 
 //
